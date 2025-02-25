27086e1dcbc6c09f28901d5f9d2303b8cd87ccef
Satish jassal <thesatishjassal@gmail.com>
2025-02-25 13:06:28 +0530

    code

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from app.models.Invoucher.invoucher import Invoucher
 from app.models.Invoucher.invoucher_item import InvoucherItem
-from app.models.products import Product
+from app.models.products import Products
 # Import other models as need
