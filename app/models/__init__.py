15c7e87f69794e2c710eea66a61544f6bd70c9a7
Satish jassal <thesatishjassal@gmail.com>
2025-02-25 17:09:51 +0530

    code

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 # app/models/__init__.py
 from app.models.clients import Clients
-from app.models.products import Product
+from app.models.products import Products
 from app.models.Invoucher.invoucher import Invoucher
 from app.models.Invoucher.invoucher_item import InvoucherItem
 from app.models.user import User  # Assuming this exists
