1012937224a24637a8cae5a936b0a798b4c41b86
Satish jassal <thesatishjassal@gmail.com>
2025-02-25 13:05:14 +0530

    code

--- app/models/products.py
+++ app/models/products.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from base import Base  # Adjust the import based on your project structure
 
-class Product(Base):
+class Products(Base):
     __tablename__ = "products"
     
     id = Column(Integer, primary_key=True, index=True)
