2a906e5bc4e746ffabb875ffcecca7d4f672c758
Satish jassal <thesatishjassal@gmail.com>
2025-04-23 16:58:21 +0530

    first commit

--- app/controllers/woocommerce.py
+++ app/controllers/woocommerce.py
@@ -4,9 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-WC_URL = os.getenv("WC_URL")
-WC_KEY = os.getenv("WC_KEY")
-WC_SECRET = os.getenv("WC_SECRET")
+# WC_URL = os.getenv("WC_URL")
+# WC_KEY = os.getenv("WC_KEY")
+# WC_SECRET = os.getenv("WC_SECRET")
+WC_URL="https://panvik.com/wp-json/wc/v3/products"
+WC_KEY="ck_82aacfc6104ebff60318341d9f78f9a3308ebc89"
+WC_SECRET="cs_79dc5a69789936c2077eb34adab9f5f80ba4d20c"
 
 def upload_products_to_woocommerce(products):
     for product in products:
