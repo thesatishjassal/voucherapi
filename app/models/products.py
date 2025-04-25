0aa84cf839244d8b6fe0e4632ab5facbb2153f70
Satish jassal <thesatishjassal@gmail.com>
2025-04-25 12:28:48 +0530

    first commit

--- app/models/products.py
+++ app/models/products.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Float, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from base import Base
 
