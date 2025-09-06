e8f61d65dd3193e69fc4fc728342692cc5e7e461
Satish jassal <thesatishjassal@gmail.com>
2025-09-06 16:53:12 +0530

    first commit

--- app/controllers/Outvoucher.py
+++ app/controllers/Outvoucher.py
@@ -42,7 +42,7 @@ def create_outvoucher_item(db: Session, voucher_id: int, item: OutvoucherItemCre
         raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
     
 def get_items_by_voucher_id(db: Session, id: str) -> List[OutvoucherItem]:
-    items = db.query(OutvoucherItem).filter(OutvoucherItem.id == int(id)).all()
+    items = db.query(OutvoucherItem).filter(OutvoucherItem.voucher_id == int(id)).all()
     if not items:
         raise HTTPException(status_code=404, detail="No items found for this voucher ID")
     return items
