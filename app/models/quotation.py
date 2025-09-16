1df3072c57c843a3b99d285e7bda8eb0ede432d1
Satish jassal <thesatishjassal@gmail.com>
2025-09-16 18:08:15 +0530

    first commit

--- app/models/quotation.py
+++ app/models/quotation.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, Date, ForeignKey, func
 from sqlalchemy.orm import relationship
 from database import Base
 
