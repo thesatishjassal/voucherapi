52ca3a61e66345194e3a0855553b0c11cfc9ca5f
Satish jassal <thesatishjassal@gmail.com>
2025-10-20 15:26:11 +0530

    purchase order api

--- app/models/products.py
+++ app/models/products.py
@@ -6,8 +6,7 @@ class Products(Base):
     __tablename__ = "products"
     
     id = Column(Integer, primary_key=True, index=True)
-    # hsncode = Column(String, unique=True)
-    itemcode = Column(String(50), nullable=False)
+    itemcode = Column(String(50), nullable=False, unique=True)
     itemname = Column(String)
     description = Column(String)
     category = Column(String)
@@ -23,19 +22,18 @@ class Products(Base):
     unit = Column(String)
     reorderqty = Column(Integer, nullable=True)
 
-    # ✅ New optional fields (text type)
+    # Optional technical fields
     cct = Column(String, nullable=True)
     beamangle = Column(String, nullable=True)
     cutoutdia = Column(String, nullable=True)
     cri = Column(String, nullable=True)
     lumens = Column(String, nullable=True)
     watt = Column(String, nullable=True)
-    # ✅ Optional field
     in_display = Column(Boolean, nullable=True, default=True)
 
     # Relationships
+    purchaseitems = relationship("PurchaseOrderItems", back_populates="product")
     items = relationship("InvoucherItem", back_populates="product")  
     salesitems = relationship("SalesorderItems", back_populates="product")  
-    # purchaseitems = relationship("SalesorderItems", back_populates="product")  
     outvoucher_items = relationship("OutvoucherItem", back_populates="product")
     quotation_items = relationship("QuotationItem", back_populates="product")
