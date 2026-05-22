2d7a66310fd4ab19a04a3324e2059f7884cc514a
Satish jassal <thesatishjassal@gmail.com>
2026-05-22 16:17:35 +0530

    login arch

--- app/utils/arch_security.py
+++ app/utils/arch_security.py
@@ -1,16 +1,33 @@
 import random
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
-
+# -------------------------
+# GENERATE OTP
+# -------------------------
 def generate_otp():
-    return str(random.randint(100000, 999999))
+    return str(
+        random.randint(100000, 999999)
+    )
 
-
+# -------------------------
+# HASH OTP
+# -------------------------
 def hash_otp(otp: str):
     return pwd_context.hash(otp)
 
-
-def verify_otp(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+# -------------------------
+# VERIFY OTP
+# -------------------------
+def verify_otp(
+    plain_otp: str,
+    hashed_otp: str
+):
+    return pwd_context.verify(
+        plain_otp,
+        hashed_otp
+    )
